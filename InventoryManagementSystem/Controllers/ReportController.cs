fba908c6076a425d6d334f730a91d694b8b617f7
Ruwanya Hettiarachchi <ruwanyahettiarachchi@gmail.com>
2025-07-31 12:55:24 +0530

    Minor changes

--- InventoryManagementSystem/Controllers/ReportController.cs
+++ InventoryManagementSystem/Controllers/ReportController.cs
@@ -1,6 +1,7 @@
 ﻿using InventoryManagementSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
 
 namespace InventoryManagementSystem.Controllers
 {
@@ -13,19 +14,47 @@ namespace InventoryManagementSystem.Controllers
 			_context = context;
 		}
 
-		public IActionResult Index()
-		{
-			var salesSummary = _context.Sales
-				.Include(s => s.Product)
-				.ThenInclude(p => p.Category)
-				.GroupBy(s => s.SaleDate.Date)
-				.Select(g => new {
-					Date = g.Key,
-					TotalSales = g.Sum(x => x.QuantitySold * x.Product.Price)
-				}).ToList();
+        public IActionResult Index()
+        {
+            var report = _context.Sales
+                .Include(s => s.Product)
+                .GroupBy(s => s.SaleDate.Date)
+                .Select(g => new SalesReportViewModel
+                {
+                    Date = g.Key,
+                    TotalSales = g.Sum(s => s.QuantitySold * s.Product.Price)
+                }).ToList();
 
-			return View(salesSummary);
-		}
-	}
+            return View(report);
+        }
+
+        public IActionResult ExportToExcel()
+        {
+            var salesData = _context.Sales
+                .Include(s => s.Product)
+                .GroupBy(s => s.SaleDate.Date)
+                .Select(g => new {
+                    Date = g.Key.ToShortDateString(),
+                    TotalSales = g.Sum(s => s.QuantitySold * s.Product.Price)
+                }).ToList();
+
+            using var package = new ExcelPackage();
+            var worksheet = package.Workbook.Worksheets.Add("SalesReport");
+
+            worksheet.Cells[1, 1].Value = "Date";
+            worksheet.Cells[1, 2].Value = "Total Sales";
+
+            for (int i = 0; i < salesData.Count; i++)
+            {
+                worksheet.Cells[i + 2, 1].Value = salesData[i].Date;
+                worksheet.Cells[i + 2, 2].Value = salesData[i].TotalSales;
+            }
+
+            var stream = new MemoryStream(package.GetAsByteArray());
+            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "SalesReport.xlsx");
+        }
+
+
+    }
 
 }
---
+++ InventoryManagementSystem/Models/SalesReportViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryManagementSystem.Models
+{
+    public class SalesReportViewModel
+    {
+        public DateTime Date { get; set; }
+        public decimal TotalSales { get; set; }
+    }
+
+}
